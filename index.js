91149efeebdfaf298b54921c6811f1e666498698
Karol Ramirez <iamacassieot5@gmail.com>
2015-06-24 06:46:29 +0000

    text-formatting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 var express    = require('express');		// call express
 var app        = express(); 				// define our app using express
 var port = process.env.PORT || 8080
-var ip = process.env.IP || "1270.0.1"
+var ip = process.env.IP || "127.0.0.1"
 
 // configure our app to handle CORS requests
 app.use(function(req, res, next) {
@@ -21,5 +21,5 @@ app.use(express.static(__dirname + '/public'));
 
 // START THE SERVER
 // ====================================
-app.listen(port);
-console.log('Magic happens on port ' + port 
+app.listen(port,  ip);
+console.log('Magic happens on port ' + port);
